89b24b483b0ef98cf5d3231d9d68123df3bec5fb
Diego Alejandro Salvado <diegosalvadodev@gmail.com>
2026-02-19 10:34:02 -0300

    Mejora de carga de video y las fuentes

--- src/components/Video.jsx
+++ src/components/Video.jsx
@@ -1,18 +1,33 @@
-import React from 'react';
-import "../assets/css/video.css"
+import React, { useState } from "react";
+import "../assets/css/video.css";
 
 export default function Video({ videoId }) {
-    return (
-    <div className="video-container">
-        <iframe
-            title="video"
-            width="100%"
-            height="100%"
-            src={`https://www.youtube.com/embed/${videoId}?&controls=0&showinfo=0&loop=1&rel=0`}
-            frameBorder="0"
-            allowFullScreen
-        ></iframe>
-    </div>
-);
-}
+  const [loaded, setLoaded] = useState(false);
 
+  const thumbnail = `https://i.ytimg.com/vi/${videoId}/hqdefault.jpg`;
+
+  return (
+    <div className="video-container">
+      {!loaded ? (
+        <div className="video-preview" onClick={() => setLoaded(true)}>
+          <img
+            src={thumbnail}
+            alt="Video preview"
+            className="video-thumbnail"
+          />
+          <div className="video-play-button">▶</div>
+        </div>
+      ) : (
+        <iframe
+          title="video"
+          width="100%"
+          height="100%"
+          src={`https://www.youtube.com/embed/${videoId}?autoplay=1&controls=0&loop=1&rel=0`}
+          frameBorder="0"
+          allow="autoplay; encrypted-media"
+          allowFullScreen
+        ></iframe>
+      )}
+    </div>
+  );
+}
--- src/containers/Home.jsx
+++ src/containers/Home.jsx
@@ -1,41 +1,41 @@
-import React from 'react';
+import React from "react";
 import "../assets/css/Home.css";
-import Video from '../components/Video';
-import AudiovisualHome from '../components/AudiovisualHome'
-import FotografiaHome from '../components/FotografiaHome';
-import Llamado from  '../components/Llamado'
-import Somos from '../components/Somos'
-import Ubicacion from '../components/Ubicacion'
-import Formulario from '../components/Formulario'
+import Video from "../components/Video";
+import AudiovisualHome from "../components/AudiovisualHome";
+import FotografiaHome from "../components/FotografiaHome";
+import Llamado from "../components/Llamado";
+import Somos from "../components/Somos";
+import Ubicacion from "../components/Ubicacion";
+import Formulario from "../components/Formulario";
 
 export default function Home() {
-    const videoId = 'dbcc_1t0in4';
+  const videoId = "dbcc_1t0in4";
 
-    return (
-        <>
-            <div className="video-container">
-                <Video videoId={videoId} />
-            </div>
-            <div className="llamado">
-                <Llamado></Llamado>
-            </div>
-            <div className="wraper">
-                <div className="audiovisual-home">
-                    <AudiovisualHome></AudiovisualHome>
-                </div>
-                <div className="fotografia-home">
-                    <FotografiaHome></FotografiaHome>
-                </div>
-            </div>
-            <div className="somos">
-                <Somos></Somos>
-            </div>
-            <div className="ubicacion">
-                <Ubicacion></Ubicacion>
-            </div>
-            <div className="contacto">
-                <Formulario></Formulario>
-            </div>
-        </>
-    );
+  return (
+    <>
+      <div className="video-container">
+        <Video videoId={videoId} />
+      </div>
+      <div className="llamado">
+        <Llamado></Llamado>
+      </div>
+      <div className="wraper">
+        <div className="audiovisual-home">
+          <AudiovisualHome></AudiovisualHome>
+        </div>
+        <div className="fotografia-home">
+          <FotografiaHome></FotografiaHome>
+        </div>
+      </div>
+      <div className="somos">
+        <Somos></Somos>
+      </div>
+      <div className="ubicacion">
+        <Ubicacion></Ubicacion>
+      </div>
+      <div className="contacto">
+        <Formulario></Formulario>
+      </div>
+    </>
+  );
 }
